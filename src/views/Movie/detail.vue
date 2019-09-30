458f6eff3c4fe944f9e855eed681a38e0a681a51
9240 <z20159240@gmail.com>
2019-09-30 11:07:53 +0800

    bug clear

--- src/views/Movie/detail.vue
+++ src/views/Movie/detail.vue
@@ -6,7 +6,7 @@
 		<Loading v-if="isLoading" />
 		<div v-else id="content" class="contentDetail">
 			<div class="detail_list">
-				<div class="detail_list_bg"></div>
+				<div class="detail_list_bg" :style="{'background-image':'url('+movieDetail.img.replace(/w\.h/,'140.208')+')'}"></div>
 				<div class="detail_list_filter"></div>
 				<div class="detail_list_content">
 					<div class="detail_list_img">
@@ -28,9 +28,9 @@
 			<div class="detail_player swiper-container" ref="detail_player">
 				<ul class="swiper-wrapper">
 					<li v-for="(item,index) in movieDetail.photos" :key="index" class="swiper-slide">
-						<!-- <div> -->
+						<div>
 							<img :src="item | setWH('70.127')" alt="">
-						<!-- </div> -->
+						</div>
 					</li>
 				</ul>
 			</div>
@@ -88,7 +88,7 @@ export default {
 }
 #content.contentDetail{ display: block; margin-bottom:0;}
 #content .detail_list{ height:200px; width:100%; position: relative; overflow: hidden;}
-.detail_list .detail_list_bg{ width:100%; height:100%; background: url(/images/movie_1.jpg) 0 40%; filter: blur(20px); background-size:cover; position: absolute; left: 0; top: 0;}
+.detail_list .detail_list_bg{ width:100%; height:100%; filter: blur(20px); background-size:cover; position: absolute; left: 0; top: 0;}
 .detail_list .detail_list_filter{ width:100%; height:100%; position: absolute;background-color: #40454d;opacity: .55; position: absolute; left: 0; top: 0; z-index: 1;}
 .detail_list .detail_list_content{ display: flex; width:100%; height:100%; position: absolute; left: 0; top: 0; z-index: 2;}
 .detail_list .detail_list_img{ width:108px; height: 150px; border: solid 1px #f0f2f3; margin:20px;}
